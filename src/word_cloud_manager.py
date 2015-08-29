8817e624931365a4c7f08ab258cf2137be341173
Tomer Saban <sab30226@gmail.com>
2015-08-29 16:56:47 +0300

    Removed the label from the word_cloud_manager and now the size of output image is 600*800

--- src/word_cloud_manager.py
+++ src/word_cloud_manager.py
@@ -15,23 +15,19 @@ def generate_cloud(person_file_path, person):
     wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
     plt.figure()
     plt.imshow(wordcloud)
-    plt.suptitle(person, fontsize=20, fontweight='bold')
+    # plt.suptitle(person, fontsize=20, fontweight='bold')
     # plt.show()
+    plt.tight_layout()
     plt.savefig(person_output_file_path)
     plt.close('all')
 
-def create_world_cloud(output_person_input_for_word_cloud_directory, \
-                       output_person_word_cloud, \
-                       output_year_word_cloud):
+def create_world_cloud(output_person_input_for_word_cloud_directory):
     for file in os.listdir(output_person_input_for_word_cloud_directory):
         person_file_path = os.path.join(output_person_input_for_word_cloud_directory, file)
         generate_cloud(person_file_path, file)
 
 
-def main(output_person_input_for_word_cloud_directory, \
-         output_person_word_cloud, \
-         output_year_word_cloud):
+def main(output_person_word_cloud, output_year_word_cloud):
 
-    create_world_cloud(output_person_input_for_word_cloud_directory, \
-                       output_person_word_cloud, \
-                       output_year_word_cloud)
+    create_world_cloud(output_person_word_cloud)
+    create_world_cloud(output_year_word_cloud)
